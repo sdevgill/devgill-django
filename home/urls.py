8578be70fada051bbeaa9088a8c8c6aa4a88ca94
Dev Gill <26435947+sdevgill@users.noreply.github.com>
2022-09-12 21:20:21 +0100

    Extend blog views to home

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
-from . import views
+from .views import HomePageView
 
+# When using class-based views, we always add as_view() to the end of the view name
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", HomePageView.as_view(), name="index"),
 ]
+
+
+# FBV
+# from . import views
+
+# urlpatterns = [
+#     path("", views.index, name="index"),
+# ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-# Create your views here.
-def index(request):
-    return render(request, "index.html")
+from blog.models import Post
+
+
+class HomePageView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["post_list"] = Post.objects.all()
+        return context
+
+
+# FBV
+# from django.shortcuts import render
+# from django.http import HttpResponse
+
+# def index(request):
+#     return render(request, "index.html")
