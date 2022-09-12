bef801b33c86bfdc0d5c6705c1d4f5ef7044d74f
Dev Gill <26435947+sdevgill@users.noreply.github.com>
2022-09-13 00:53:31 +0100

    Add basic styling

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,7 @@ class Post(models.Model):
         on_delete=models.CASCADE,
     )
     body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
