6e692cd766908ab5a1273e7cb3e87a09513bd274
Dev Gill <26435947+sdevgill@users.noreply.github.com>
2022-09-18 01:08:32 +0100

    Add home app tests

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomePageTests(TestCase):
+    def test_url_exists_at_correct_location_homepageview(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_view(self):
+        response = self.client.get(reverse("index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "index.html")
+        self.assertContains(response, "Home")
